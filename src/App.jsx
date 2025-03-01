d47c244cebacc5f82f2cfb03ee9c80705bd42b5e
Saung Eaindray Min <saungeaindraymin@gmail.com>
2025-03-01 12:47:34 +0630

    api binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { PokeCard } from "./components/PokeCard";
 import { SideNav } from "./components/SideNav";
 
+
+
 function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
   return (
     <>
-    <Header/>
-    <SideNav/>
-    <PokeCard/>
+      <Header />
+      <SideNav
+        selectedPokemon={selectedPokemon}
+        setSelectedPokemon={setSelectedPokemon}
+      />
+      <PokeCard selectedPokemon={selectedPokemon} />
     </>
   );
 }
